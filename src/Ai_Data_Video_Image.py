fb3c671ef525685bac3d64f15453735f958602f4
qi11028 <Xiangwen.Sun@telemotive.de>
2017-11-29 17:30:41 +0100

    new

--- src/Ai_Data_Video_Image.py
+++ src/Ai_Data_Video_Image.py
@@ -31,13 +31,18 @@ class Video_Utils(object):
               count += 1    
               
 class Facial_Reg(object):
+    clsStr_ClassifierFace = r"F:\SunsNoteBook\Data\classifiers\20046\haarcascades\haarcascade_frontalface_default.xml"
+    clsStr_ClassifierEye = r"F:\SunsNoteBook\Data\classifiers\20046\haarcascades\haarcascade_eye.xml"
+    clsStr_ClassifierEye_1 = r"F:\SunsNoteBook\Data\classifiers\20046\haarcascades\haarcascade_eye_tree_eyeglasses.xml"
+    clsStr_ClassifierEye_2 = r"F:\SunsNoteBook\Data\classifiers\20046\haarcascades_cuda\haarcascade_eye_tree_eyeglasses.xml"
+    clsStr_ClassifierEye_3 = r"F:\SunsNoteBook\Data\classifiers\cascades\haarcascade_mcs_eyepair_small.xml"
     
     def __init__(self):
         pass
     
     def test_f_1(self):
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+        face_cascade = cv2.CascadeClassifier(Facial_Reg.clsStr_ClassifierFace.replace('\\', '/'))
+        eye_cascade = cv2.CascadeClassifier(Facial_Reg.clsStr_ClassifierEye_3.replace('\\', '/'))
 
         listMatchedFiles = OSDirectoryUtils.get_Dir_Files_with_Extention(Video_Utils.clsStr2.replace('\\', '/'), '.jpg')
         for fname in listMatchedFiles:
@@ -55,6 +60,25 @@ class Facial_Reg(object):
             cv2.imshow('img',img)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+            
+    def test_f_2(self):
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+        eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+        
+        img = cv2.imread('sachin.jpg') 
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #@endcode Now we find the faces in the image. If faces are found, it returns the positions of detected faces as Rect(x,y,w,h). Once we get these locations, we can create a ROI for the face and apply eye detection on this ROI (since eyes are always on the face !!! ). @code{.py} 
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+        for (x,y,w,h) in faces: 
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) 
+            roi_gray = gray[y:y+h, x:x+w] 
+            roi_color = img[y:y+h, x:x+w] 
+            eyes = eye_cascade.detectMultiScale(roi_gray) 
+            for (ex,ey,ew,eh) in eyes: 
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        
+        cv2.imshow('img',img) 
+        cv2.waitKey(0) 
+        cv2.destroyAllWindows() 
 
 if __name__ == '__main__':
     #oVideo_Utils = Video_Utils()
